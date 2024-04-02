61ff06280584f951b78941657108fe41c36ec75b
mairaafreitas <maira.oliveirafreitas@gmail.com>
2024-04-02 08:30:11 -0300

    feat: add bandwith calculator

---
+++ bandwith_calculator.py
@@ -0,0 +1,12 @@
+from utils import kb_to_mb
+
+
+def calculate_bandwith(requests_per_second: int, request_size: int):
+    """Calculate the Bandwith required for the requests."""
+    print("\n======Bandwith======")
+    print("Bandwith = Requests per second * Average request size")
+
+    bandwith_kb = round(requests_per_second * request_size, 4)
+    bandwith_mb = round(kb_to_mb(bandwith_kb), 4)
+    print(f"The Bandwith required for the requests is: {bandwith_kb}kbs")
+    print(f"The Bandwith required for the requests is: {bandwith_mb}mbs")
